6af700777ae054b89252f44067c005b15008865c
Paul Caponetti <pcaponetti@PCAPONETTI-MBP.local>
2015-07-21 15:38:29 -0400

    added grunt watcher, concat and minification

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		concat:{
+			dist: {
+				src: [
+					'js/directives/mqttControls.js',
+					'js/directives/mqttBar.js',
+					'js/directives/mqttDoughnut.js',
+				],
+				dest: 'dist/mqttControls.full.js'
+			}
+		},
+		watch: {
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['concat', 'uglify'],
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'dist/mqttControls.min.js': [
+						'js/directives/mqttControls.js',
+						'js/directives/mqttBar.js',
+						'js/directives/mqttDoughnut.js',
+					]
+				}
+			}
+		}
+  	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['watch']);
+};
