c2f90699bfe33a7311ba5a043fa4acddab48c11a
Paul Caponetti <pcaponetti@PCAPONETTI-MBP.local>
2015-07-21 15:18:27 -0400

    setting up grunt

--- js/directives/mqttSwitch.js
+++ js/directives/mqttSwitch.js
@@ -1,7 +1,1 @@
-app.directive('mqttSwitch', function(){
-  return {
-    restrict: 'AE',
-    require: '^ngModel',
-    template: "<div style='display:inline-block; width: 40px; background-color: #3333ff; height:{{temp}}px'>Temp</div>"
-  }
-});
+
