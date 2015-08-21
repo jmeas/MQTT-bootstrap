d869b982bcabb15b75d8c7d1b294d23dd517febe
Paul Caponetti <paul.caponetti@gmail.com>
2015-08-21 14:06:22 -0400

    added pub queue test

--- tests/mqtt.services.tests.js
+++ tests/mqtt.services.tests.js
@@ -1,4 +1,4 @@
-describe("A test suite", function() {
+describe("Pub sub suite", function() {
   beforeEach(module("Mqtt.Services"));
   var msg = '';
   beforeEach('make sure you get the sub when you pub', inject(function(mqtt) {
@@ -11,6 +11,25 @@ describe("A test suite", function() {
   it('make sure you get the sub when you pub', function(done) {
     setTimeout(function() {
       expect(msg).equals('hi');
+      done();
+    }, 1500);
+  })
+});
+
+
+describe("Sub queueing suite", function() {
+  beforeEach(module("Mqtt.Services"));
+  var msg = '';
+  beforeEach('make sure you queue up pubs before connect', inject(function(mqtt) {
+    mqtt.subscribe('testing-mqtt-dashboard', function(message) {
+      msg = message.payloadString;
+    }, 'broker.mqttdashboard.com', 8000);
+    mqtt.connect('broker.mqttdashboard.com', 8000);
+    mqtt.sendMessage('hey there', 'testing-mqtt-dashboard', 'broker.mqttdashboard.com', 8000);
+  }));
+  it('make sure you get the sub when you pub', function(done) {
+    setTimeout(function() {
+      expect(msg).equals('hey there');
       done();
     }, 1500);
   })
